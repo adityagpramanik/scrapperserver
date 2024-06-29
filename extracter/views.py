3a1221a8c6a57b074120f4840adf6953603ad081
adityagpramanik <adityagpramanik@gmail.com>
2024-06-30 02:29:40 +0530

    add pdfminer.six lib to extract text from pdf change 400 error status code 403 (bad request)

--- extracter/views.py
+++ extracter/views.py
@@ -10,19 +10,20 @@ def github(request):
 		try:
 			data = json.loads(request.body)
 			if not data['username']:
-				return JsonResponse({'error': 'Invalid username'}, status=400)
+				return JsonResponse({'error': 'Invalid username'}, status=403)
 			return JsonResponse(_github.fetch(data['username']))
-		except json.JSONDecodeError as e:
-			return JsonResponse({'error': 'Invalid JSON'}, status=400)
+		except json.JSONDecodeError:
+			return JsonResponse({'error': 'Invalid JSON'}, status=403)
 	elif request.method == 'GET':
 		try:
 			username = request.GET['username']
 			if not username:
-				return JsonResponse({'error': 'Invalid username'}, status=400)
+				return JsonResponse({'error': 'Invalid username'}, status=403)
 			
 			result = _github.fetch(username)
 			return JsonResponse(result)
 		except:
-			return JsonResponse({'error': 'Invalid JSON'}, status=400)
+			return JsonResponse({'error': 'Invalid JSON'}, status=403)
 	else:
-		return JsonResponse({'error': 'Only POST method allowed'}, status=405)
+		return JsonResponse({'error': 'Method not allowed for this endpoint'}, status=405)
+
